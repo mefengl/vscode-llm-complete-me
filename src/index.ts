089b77256839f61933839e705a6a80f8ffd3bbf5
mefengl <71683364+mefengl@users.noreply.github.com>
2025-01-27 06:57:39 +0800

    command added

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
-import { defineExtension } from 'reactive-vscode'
+import { defineExtension, useCommand } from 'reactive-vscode'
 import { window } from 'vscode'
 
-const { activate, deactivate } = defineExtension(() => {
-  window.showInformationMessage('Hello')
-})
+const { activate, deactivate } = defineExtension(() => useCommand('llm-compelete-me', () => {
+  window.showInformationMessage(context() || 'No text available')
+}))
 
 export { activate, deactivate }
+
+function context() {
+  const activeTextEditor = window.activeTextEditor
+  // if have selection, return selection
+  const selectionText = activeTextEditor?.document.getText(activeTextEditor?.selection)
+  if (selectionText)
+    return selectionText
+  // else return all text in active editor
+  return activeTextEditor?.document.getText()
+}
