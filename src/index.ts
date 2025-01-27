8612439dbc7d8d5bf414a6925783228d5b4a2b66
mefengl <71683364+mefengl@users.noreply.github.com>
2025-01-28 05:50:29 +0800

    feat: config

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,31 @@
-import { defineExtension, useCommand } from 'reactive-vscode'
+import { defineConfigObject, defineExtension, useCommand } from 'reactive-vscode'
 import vscode from 'vscode'
+import { ofetch } from 'ofetch'
 import { logger } from './utils'
 
+const config = defineConfigObject('llm-compelete-me', { useCopilot: Boolean, baseURL: [String, null], apiKey: [String, null], model: [String, null] })
+
 const { activate, deactivate } = defineExtension(() => useCommand('llm-compelete-me.please', async () => {
+  if (!config.useCopilot && (!config.baseURL || !config.apiKey || !config.model)) {
+    const baseURL = await vscode.window.showInputBox({ prompt: 'Enter the base URL of the language model, or enter "copilot" if you have copilot subscription', value: config.baseURL ?? undefined })
+    if (!baseURL)
+      return vscode.window.showInformationMessage('No base URL provided')
+    if (baseURL === 'copilot') {
+      config.useCopilot = true
+    }
+    else {
+      config.baseURL = baseURL.trim().replace(/\/$/, '')
+      const apiKey = await vscode.window.showInputBox({ prompt: 'Enter the API key of the language model', value: config.apiKey ?? undefined })
+      if (!apiKey)
+        return vscode.window.showInformationMessage('No API key provided')
+      config.apiKey = apiKey
+      const model = await vscode.window.showInputBox({ prompt: 'Enter the model of the language model', value: config.model ?? undefined })
+      if (!model)
+        return vscode.window.showInformationMessage('No model provided')
+      config.model = model
+    }
+  }
+
   const activeTextEditor = vscode.window.activeTextEditor
   if (!activeTextEditor)
     return vscode.window.showInformationMessage('No active text editor')
@@ -10,7 +33,10 @@ const { activate, deactivate } = defineExtension(() => useCommand('llm-compelete
   if (!context)
     vscode.window.showInformationMessage('No text available')
   await activeTextEditor.edit(edit => edit.insert(activeTextEditor.selection.active, '\n'))
-  vscode.window.withProgress({ title: 'Compeleting...', location: vscode.ProgressLocation.Notification }, async () => await giveMeAnswer(activeTextEditor, context))
+  if (config.useCopilot)
+    await vscode.window.withProgress({ title: 'Compeleting...', location: vscode.ProgressLocation.Notification }, async () => await giveMeAnswerCopilot(activeTextEditor, context))
+  else
+    await vscode.window.withProgress({ title: 'Compeleting...', location: vscode.ProgressLocation.Notification }, async () => await getAnswer(activeTextEditor, context))
   await activeTextEditor.edit(edit => edit.insert(activeTextEditor.selection.active, '\n'))
 }))
 
@@ -26,7 +52,7 @@ function getContext(textEditor: vscode.TextEditor) {
 }
 
 // ref https://code.visualstudio.com/api/extension-guides/language-model#interpret-the-response
-async function giveMeAnswer(textEditor: vscode.TextEditor, q: string) {
+async function giveMeAnswerCopilot(textEditor: vscode.TextEditor, q: string) {
   const [model] = await vscode.lm.selectChatModels({ vendor: 'copilot', family: 'claude-3.5-sonnet' })
   let chatResponse: vscode.LanguageModelChatResponse | undefined
   const messages = [vscode.LanguageModelChatMessage.User(q)]
@@ -40,10 +66,8 @@ async function giveMeAnswer(textEditor: vscode.TextEditor, q: string) {
       throw err
   }
 
-  if (!chatResponse) {
-    vscode.window.showInformationMessage('No response from the language model')
-    return
-  }
+  if (!chatResponse)
+    return vscode.window.showInformationMessage('No response from the language model')
   try {
     // Stream the code into the editor as it is coming in from the Language Model
     for await (const fragment of chatResponse.text)
@@ -52,5 +76,22 @@ async function giveMeAnswer(textEditor: vscode.TextEditor, q: string) {
   catch (err) {
     // async response stream may fail, e.g network interruption or server side error
     await textEditor.edit(edit => edit.insert(textEditor.selection.active, (<Error>err).message))
+  }
+}
+
+async function getAnswer(textEditor: vscode.TextEditor, context: string) {
+  try {
+    const res = await ofetch(`${config.baseURL}/chat/completions`, {
+      body: JSON.stringify({ messages: [{ content: context, role: 'user' }], model: config.model }),
+      headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${config.apiKey}` },
+      method: 'POST',
+    })
+    const chatResponse = res?.choices?.[0]?.message
+    if (!chatResponse)
+      return vscode.window.showInformationMessage('No response from the language model')
+    await textEditor.edit(edit => edit.insert(textEditor.selection.active, chatResponse.content))
+  }
+  catch (err) {
+    return vscode.window.showInformationMessage((<Error>err).message)
   }
 }
